1c46561b3f8271e13fd569594b45ced8e96bb765
ilushacomeback <ilya.rudenko.2105@mail.ru>
2023-05-31 22:02:15 +0300

    add brands-list_item:hover

--- main.js
+++ main.js
@@ -13,22 +13,22 @@
     }
   }
   getSwiper(width)
+
+  const button = document.querySelector(".btn")
+  button.addEventListener("click", function () {
+    button.classList.toggle("btn_toggle")
+  })
+
+  const btn = document.querySelector(".btn > span")
+  btn.addEventListener("click", function () {
+    btn.innerHTML =
+      btn.innerHTML === "Показать всё"
+        ? (btn.innerHTML = "Скрыть")
+        : (btn.innerHTML = "Показать всё")
+  })
+
+  const next_1120px = document.querySelector(".brands-list")
+  button.addEventListener("click", function () {
+    next_1120px.classList.toggle("brands-list__toggle")
+  })
 })()
-
-const button = document.querySelector(".btn")
-button.addEventListener("click", function () {
-  button.classList.toggle("btn_toggle")
-})
-
-const btn = document.querySelector(".btn > span")
-btn.addEventListener("click", function () {
-  btn.innerHTML =
-    btn.innerHTML === "Показать всё"
-      ? (btn.innerHTML = "Скрыть")
-      : (btn.innerHTML = "Показать всё")
-})
-
-const next_1120px = document.querySelector(".brands-list")
-button.addEventListener("click", function () {
-  next_1120px.classList.toggle("brands-list__toggle")
-})
